34042b761de5895fd3c452ed0ae87e089b24f89e
circle0707 <123530411+circle0707@users.noreply.github.com>
2025-09-15 20:02:50 +0900

    Enemy Moving and Attack Motion

--- Assets/01_Scripts/Enemy/Dragun/Dragun.cs
+++ Assets/01_Scripts/Enemy/Dragun/Dragun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Dragun : MonoBehaviour
 {
     Rigidbody ri;
-    Animator animer;
+    [SerializeField] Animator animer;
 
     [SerializeField] GameObject target;
     [SerializeField] float dragunSpeed;
@@ -15,23 +15,22 @@ public class Dragun : MonoBehaviour
 
     private void Start()
     {
-        animer = GetComponent<Animator>();
         ri = GetComponent<Rigidbody>();
         target = GameObject.Find("Player");
     }
 
-    void Update()
+    void FixedUpdate()
     {
         RayToPlayer();
         if (findRay == true)
         {
             FaceToPlayer();
-            //Attack();
+            Attack();
         }
         if (findRay == false)
         {
             FaceToPlayer();
-            //moving();
+            Moving();
         }
     }
 
@@ -46,13 +45,13 @@ public class Dragun : MonoBehaviour
     void RayToPlayer()
     {
         Vector3 dir = target.transform.position - transform.position; dir.y = 0f;
-        findRay = Physics.Raycast(ri.position, dir.normalized, 10, LayerMask.GetMask("Player"));
-        Debug.DrawRay(ri.position, dir.normalized * 10, Color.red);
+        findRay = Physics.Raycast(ri.position, dir.normalized, 1, LayerMask.GetMask("Player"));
+        Debug.DrawRay(ri.position, dir.normalized * 1, Color.red);
     }
 
-    void moving()
+    void Moving()
     {
-        animer.SetBool("attack", false);
+        animer.SetBool("Attack", false);
         Vector3 vel = transform.forward * dragunSpeed;
         vel.y = ri.linearVelocity.y;
         ri.linearVelocity = vel;
@@ -67,15 +66,15 @@ public class Dragun : MonoBehaviour
     {
         if (canAttack == true)
         {
-            animer.SetBool("attack", true);
-            Instantiate(thingOfAttack, this.transform.position, this.transform.rotation);
+            animer.SetBool("Attack", true);
+            //Instantiate(thingOfAttack, this.transform.position, this.transform.rotation);
             canAttack = false;
             Invoke("Anime", 0.6f);
         }
     }
     void Anime()
     {
-        animer.SetBool("attack", false);
+        animer.SetBool("Attack", false);
         canAttack = true;
     }
 }
